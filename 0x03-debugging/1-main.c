8f2f207cfd77bbc382ec7f1fbb5c184dcf5088c1
Muthoka254 <muthokatom5@gmail.com>
2022-03-21 13:29:52 -0700

    eleven

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+/**
+*main - causes an infinite loop
+*
+*Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf(Ã("infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+*while (i < 10)
+*{
+*Infinite loop - No increase of variables
+*putchar(i);
+}
+*/
+
+printf(Ã"infinite loop avoided! \\o/\n");
+return (0);
+}
